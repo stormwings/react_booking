ad66ee38f05c2d9953b6830c49559ab56babb0d9
Mariano <marianov994@gmail.com>
2020-01-21 08:40:23 -0300

    show new components in app (prev to router)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./App.scss";
+
+import reducer from "./redux/reducers";
+import initialState from "./redux/initialState";
 
 import HeaderPanel from "./components/smart/Header/Header";
 import HeroPanel from "./components/smart/Hero/Hero";
@@ -9,6 +14,7 @@ import GuidePanel from "./components/smart/Guide/Guide";
 import DestinationsPanel from "./components/smart/Destinations/Destinations";
 import FooterPanel from "./components/smart/Footer/Footer";
 import SuggestionsPanel from "./components/smart/Suggestions/Suggestions";
+import Store from "./components/smart/Store/Store";
 
 import { currencies } from "./assets/json/currency.json";
 import { magazines } from "./assets/json/magazines.json";
@@ -17,18 +23,25 @@ import { destinations } from "./assets/json/destinations.json";
 
 import { brandUrl, signInUrl, searchUrl } from "./helpers/constants";
 import { landingQuestion, guideTexts, heroText } from "./helpers/copys";
+import Checkout from "./Checkout";
+
+const store = createStore(reducer, initialState);
 
 export default function App() {
   return (
-    <div className="App">
-      <HeaderPanel signInUrl={signInUrl} currencies={currencies} />
-      <HeroPanel logo={brandUrl} subtitle={heroText} searchUrl={searchUrl} />
-      <QuestionPanel textObject={landingQuestion} />
-      <MagazinePanel logo={brandUrl} magazines={magazines} />
-      <SuggestionsPanel suggestionsList={suggestions} />
-      <GuidePanel textObject={guideTexts} />
-      <DestinationsPanel destinations={destinations} />
-      <FooterPanel logo={brandUrl} />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Checkout />
+        <HeaderPanel signInUrl={signInUrl} currencies={currencies} />
+        <HeroPanel logo={brandUrl} subtitle={heroText} searchUrl={searchUrl} />
+        <Store />
+        <QuestionPanel textObject={landingQuestion} />
+        <MagazinePanel logo={brandUrl} magazines={magazines} />
+        <SuggestionsPanel suggestionsList={suggestions} />
+        <GuidePanel textObject={guideTexts} />
+        <DestinationsPanel destinations={destinations} />
+        <FooterPanel logo={brandUrl} />
+      </div>
+    </Provider>
   );
 }
